5d9b5a82167f0ee068a24522493838883e868be3
Happid <happidilmi@gmail.com>
2025-12-23 15:06:23 +0700

    feat: add register page and guard route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,27 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/Login.tsx";
 import TodoPage from "../pages/Todo.tsx";
+import { useAuthStore } from "../store/authStore.ts";
+import Register from "../pages/Register.tsx";
 
 export const AppRouter = () => {
+  const token = useAuthStore((state) => state.token);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/todos" element={<TodoPage />} />
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/todos" /> : <LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={token ? <Navigate to="/todos" /> : <Register />}
+        />
+        <Route
+          path="/todos"
+          element={token ? <TodoPage /> : <Navigate to="/login" />}
+        />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
