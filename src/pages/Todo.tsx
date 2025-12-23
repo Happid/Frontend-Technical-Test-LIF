8cfe1bb811310be7aafcf4c5cdbc70729d1c6d09
Happid <happidilmi@gmail.com>
2025-12-23 14:55:08 +0700

    feat: add pages and component

---
+++ src/pages/Todo.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
+const TodoPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Todo Page</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default TodoPage;
