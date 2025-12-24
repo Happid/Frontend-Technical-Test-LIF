65c0dd7e5cb828cd3090a757fe74556be2db84b3
Happid <happidilmi@gmail.com>
2025-12-24 13:10:02 +0700

    feat: adding url live demo to axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://backend-technical-test-lif-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
